d3180866dffd781304246a084a4d30e86eb4d2b7
Sunghyun Kim <hello@sunghyun.me>
2024-12-14 12:38:29 +0900

    release: v0.0.1

--- release.sh
+++ release.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# 현재 브랜치 확인
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+# master 브랜치가 아니면 master로 전환
+if [ "$current_branch" != "master" ]; then
+    echo "master 브랜치로 전환합니다..."
+    git checkout master
+fi
+
 # 현재 버전 가져오기
 current_version=$(node -p "require('./HeliosLauncher/package.json').version")
 echo "현재 버전: v$current_version"
@@ -18,7 +27,7 @@ git commit -m "release: v$new_version"
 
 # 태그 생성 및 푸시
 git tag "v$new_version"
-git push origin main
+git push origin master
 git push origin "v$new_version"
 
 echo "v$new_version 버전이 성공적으로 배포되었습니다!"
